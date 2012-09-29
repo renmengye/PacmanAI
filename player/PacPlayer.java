c76cb30bbbcf6875df79fd2bf6b717069f2280aa
renmengye <renmengye@gmail.com>
2012-09-29 11:45:16 -0400

    Initial commit

---
+++ player/PacPlayer.java
@@ -0,0 +1,97 @@
+import java.awt.Point;
+import java.util.List;
+
+import com.orbischallenge.pacman.api.common.*;
+import com.orbischallenge.pacman.api.java.*;
+
+/**
+ * The Player class is the parent class of your AI player. It is just like a
+ * template. Your AI player class (called PacPlayer) must implement the
+ * following methods.
+ * 
+ */
+public class PacPlayer implements Player {
+
+	private int lives = 3;
+
+	/**
+	 * This is method decides Pacman’s moving direction in the next frame (See
+	 * Frame Concept). The parameters represent the maze, ghosts, Pacman, and
+	 * score after the execution of last frame. In the next frame, the game will
+	 * call this method to set Pacman’s direction and let him move (see Pacman’s
+	 * Move).
+	 * 
+	 * @param maze
+	 *            A Maze object representing the current maze.
+	 * @param ghosts
+	 *            An array of Ghost objects representing the four ghosts.
+	 * @param pac
+	 *            A Pac object representing Pacman
+	 * @param score
+	 *            The current score
+	 * @return MoveDir
+	 */
+	public MoveDir calculateDirection(Maze maze, Ghost[] ghosts, Pac pac,
+			int score) {
+		MoveDir[] directions = MoveDir.values();
+
+		// Get the current tile of Pacman
+		Point pacTile = pac.getTile();
+
+		// Iterate through the four directions
+		for (MoveDir dir : directions) {
+
+			// Find Pacman's next tile if it were going on this direction
+			Point nextTile = JUtil.vectorAdd(pacTile, JUtil.getVector(dir));
+
+			// Get the maze item of that tile
+			MazeItem item = maze.getTileItem(nextTile);
+
+			// If the item is a dot or power dot, go to that tile
+			if (item == MazeItem.DOT || item == MazeItem.POWER_DOT) {
+				return dir;
+			}
+		}
+		// If we get here, just keep going on the current direction
+		return pac.getDir();
+	}
+
+	/**
+	 * This method will be called by the game whenever a new level starts. The
+	 * parameters represent the game objects at their initial states. This
+	 * method will always be called before calculateDirection.
+	 * 
+	 * @param maze
+	 *            A Maze object representing the current maze.
+	 * @param ghosts
+	 *            An array of Ghost objects representing the four ghosts.
+	 * @param pac
+	 *            A Pac object representing Pacman
+	 * @param score
+	 *            The current score
+	 */
+	public void onLevelStart(Maze maze, Ghost[] ghosts, Pac pac, int score) {
+		System.out.println("Java player start new level!");
+		MazeGraph graph = new MazeGraph(maze);
+	}
+
+	/**
+	 * This method will be called by the game whenever Pacman receives a new
+	 * life, including the first life. The parameters represent the
+	 * repositioned game objects. This method will always be called before
+	 * calculateDirection and after onLevelStart.
+	 * 
+	 * @param maze
+	 *            A Maze object representing the current maze.
+	 * @param ghosts
+	 *            An array of Ghost objects representing the four ghosts.
+	 * @param pac
+	 *            A Pac object representing Pacman
+	 * @param score
+	 *            The current score
+	 */
+	public void onNewLife(Maze maze, Ghost[] ghosts, Pac pac, int score) {
+		System.out.println("Hi, I still have " + lives + " lives left.");
+		lives--;
+	};
+}
