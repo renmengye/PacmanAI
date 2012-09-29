7a5671a4e2b2b97e78c3681c0ba34707c0f27b25
renmengye <renmengye@gmail.com>
2012-09-29 13:06:03 -0400

    Refratored classes and packages

--- run.bat
+++ run.bat
@@ -2,9 +2,9 @@
 set DIR=%CD%
 SET JAVA_HOME="C:\Program Files\Java\jdk1.7.0_07\bin"
 SET JARPATH="%CD%\pacman-java.jar"
-SET SRC="%CD%\player\PacPlayer.java"
-SET CLASS_PATH="%CD%\player"
-SET CLASS="PacPlayer"
+SET SRC="%CD%\src\Player\PacPlayer.java"
+SET CLASS_PATH="%CD%\build\classes"
+SET CLASS="Player.PacPlayer"
 
 %JAVA_HOME%\javac.exe -classpath %JARPATH%;%CLASS_PATH% %SRC% 2>&1
 if ERRORLEVEL 1 goto :eof
